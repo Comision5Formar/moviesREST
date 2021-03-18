42f74df039304ab2fdecae998281bec96c4e3ad3
EricFormar <66625345+EricFormar@users.noreply.github.com>
2021-03-18 17:46:40 -0300

    agrega create

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,29 +1,57 @@
 const db = require('../database/models');
 
+const getUrl = (req) => req.protocol + '://' + req.get('host') + req.originalUrl;
+ 
 module.exports = {
     getAll : (req,res) => {
-        const url = req.protocol + '://' + req.get('host') + req.originalUrl
 
         db.Pelicula.findAll()
         .then(peliculas => {
 
             peliculas.forEach(pelicula => {
-                pelicula.setDataValue('endpoint',url + '/' +pelicula.id)
+                pelicula.setDataValue('endpoint',getUrl(req) + '/' +pelicula.id)
             });
 
-            let data = {
+            let response = {
                 meta :{
-                    link :  url,
-                    cantidad : peliculas.length
+                    link :  getUrl(req),
+                    cantidad : peliculas.length,
+                    status : 200
                 },
                 data : peliculas
-                
             }
 
-            return res.status(200).json(data)
+            return res.status(200).json(response)
         })
     },
     getById : (req,res) => {
-
+        db.Pelicula.findByPk(req.params.id)
+        .then(pelicula => {
+            let response = {
+                meta : {
+                    link : getUrl(req),
+                    status : 200
+                },
+                data : pelicula
+            }
+            return res.status(200).json(response)
+        })
+        .catch(() => res.status(500))
+    },
+    create : (req,res) => {
+        const {title,rating,awards,release_date,length} = req.body
+        db.Pelicula.create({
+            title,
+            rating,
+            awards,
+            release_date,
+            length
+        })
+        .then(()=> {
+            return res.status(201).json({
+                msg : "la pelicula fue agregada con éxito"
+            })
+        })
+        .catch(() => res.status(500))
     }
 }
