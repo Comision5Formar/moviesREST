54b01ac6e59cd64447296345c2916082452828ac
EricFormar <66625345+EricFormar@users.noreply.github.com>
2021-03-18 17:27:09 -0300

    arreglando el matete

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,13 +1,23 @@
-const db = require('../database/models')
+const db = require('../database/models');
 
 module.exports = {
     getAll : (req,res) => {
+        const url = req.protocol + '://' + req.get('host') + req.originalUrl
+
         db.Pelicula.findAll()
         .then(peliculas => {
 
+            peliculas.forEach(pelicula => {
+                pelicula.setDataValue('endpoint',url + '/' +pelicula.id)
+            });
+
             let data = {
-                cantidad : peliculas.length,
-                peliculas
+                meta :{
+                    link :  url,
+                    cantidad : peliculas.length
+                },
+                data : peliculas
+                
             }
 
             return res.status(200).json(data)
